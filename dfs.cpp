8d197a2cb59aa8c38b2e6982437f8a6fc4a281be
Mano Sriram <mano.sriram0@gmail.com>
2020-09-18 01:03:23 +0530

    Add comments: dfs

--- dfs.cpp
+++ dfs.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// Graph
 vector<int> g[10];
+// Array to mark nodes as visited/not-visited. Initially, all nodes are
+// not-visited.
 bool visited[10] = { false };
 
+// Create an edge between src and dest.
 void create_edge(int src, int dest) {
     g[src].push_back(dest);
 }
 
+// dfs driver function.
 void dfs(int vertex) {
+    // If the current vertex is already visited, just return.
     if (visited[vertex] == true) return;
 
     cout << vertex << " ";
+
+    // Mark the vertex as visited.
     visited[vertex] = true;
+    
+    // For every child of the current vertex, recursively call dfs();
     for (auto node: g[vertex]) {
         // node -> a child of vertex;
         dfs(node);
